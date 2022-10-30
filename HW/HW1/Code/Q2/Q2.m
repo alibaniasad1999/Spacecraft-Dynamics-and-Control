dd84812747be5ae43c512c01d94f99b8af69e660
Ali <alibaniasad1999@yahoo.com>
2022-10-31 01:01:02 +0330

    add Q2

--- HW/HW1/Code/Q2/Q2.m
+++ HW/HW1/Code/Q2/Q2.m
@@ -1,7 +1,24 @@
+clear;
+clc;
+
 R_e = 6378;
 h = 600;
-r = R_e + h;
-v = 3.5; % dot(r)
 mu = 398600;
+r = R_e + h;
+v_r = 3.5; % dot(r)
+v_h = 7;
+v = sqrt(3.5^2 + 7^2);
+h = r * v_h;
+
+epsilon = v^2 / 2 - mu / r;
+
+e = sqrt(1 + 2 * epsilon * h^2/(mu^2));
+
+P = h^2 / mu;
+
+theta = acos((P-r)/r/e);
+
+
+
 
 
