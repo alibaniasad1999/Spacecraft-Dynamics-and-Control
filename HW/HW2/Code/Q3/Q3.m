e7499acd78e5fc404fd8b2a3f6f83f6637fd5f1d
Ali <alibaniasad1999@yahoo.com>
2022-12-09 22:14:23 +0330

    add Q3

--- HW/HW2/Code/Q3/Q3.m
+++ HW/HW2/Code/Q3/Q3.m
@@ -13,6 +13,7 @@ a = (r_a + r_p) / 2;
 tau = 2 * pi * sqrt(a^3 / mu);
 
 %% solve theta
+theta_0 = 0;
 theta_1 = pi/6;
 theta_2 = pi/2;
 
@@ -22,7 +23,6 @@ M_e_2 = E_calculator(theta_2, e) - e * sin(E_calculator(theta_2, e));
 t_1 = M_e_1 / 2 / pi * tau;
 t_2 = M_e_2 / 2 / pi * tau;
 
-fprintf("time part a: %f sec \n", abs(t_1 - t_2));
 delta_t = abs(t_1 - t_2);
 
 h = h_calculator(mu, r_a, r_p);
@@ -30,14 +30,16 @@ h = h_calculator(mu, r_a, r_p);
 R_1 = h^2 / mu * (1 / (1 + e * cos(theta_1))); % norm R
 R_2 = h^2 / mu * (1 / (1 + e * cos(theta_2))); % norm R
 
-r_1 = [R_1 * cos(theta_1), R_1 * sin(theta_1), 0];
+r_1 = [R_1 * cos(theta_0), R_1 * sin(theta_0), 0];
 r_2 = [R_2 * cos(theta_2), R_2 * sin(theta_2), 0];
 
 string = 'pro';
 [v1, v2] = lambert(r_1, r_2, delta_t, string);
 
-v1_o = mu/h * [-sin(theta_1), (e+cos(theta_1)), 0];
+v1_o = mu/h * [-sin(theta_0), (e+cos(theta_0)), 0];
 v2_o = mu/h * [-sin(theta_2), (e+cos(theta_2)), 0];
+
+
 
 
 
